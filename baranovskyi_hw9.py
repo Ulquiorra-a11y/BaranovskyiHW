5460f5c09358c2bd0672ea8b639a2c715e02a725
Ulquiorra-a11y <coll.sasha2013@gmail.com>
2026-02-03 12:40:36 +0100

    add task 2 and 1

--- baranovskyi_hw9.py
+++ baranovskyi_hw9.py
@@ -1,12 +1,18 @@
-n = int(input('Число: '))
+# n = int(input('Число: '))
+#
+# for i in range(1, n + 1):
+#     tmp = ''
+#     for j in range(1, i + 1):
+#         tmp += str(j)
+#     print(tmp)
 
-for i in range(1, n + 1):
-    tmp = ''
-    for j in range(1, i + 1):
-        tmp += str(j)
-    print(tmp)
-
-
-
-
+# n = int(input('Число: '))
+# x = len(str(n * n))
+# for i in range(1,n+1):
+#     tmp = ''
+#     for j in range(1,n+1):
+#         value = i * j
+#         space = x - len(str(value))
+#         print(' ' * space + str(value), end = ' ')
+#     print(tmp)
 

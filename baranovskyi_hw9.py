f4974dc54b5072f52f88286a230495a263f30abd
Ulquiorra-a11y <coll.sasha2013@gmail.com>
2026-02-03 12:22:18 +0100

    add task 2

--- baranovskyi_hw9.py
+++ baranovskyi_hw9.py
@@ -1,2 +1,12 @@
-print("hello world")
-print("hello world")
+n = int(input('Число: '))
+
+for i in range(1, n + 1):
+    tmp = ''
+    for j in range(1, i + 1):
+        tmp += str(j)
+    print(tmp)
+
+
+
+
+

cb4d535b972ff8f23bb397d136aace62b881dc70
Ulquiorra-a11y <coll.sasha2013@gmail.com>
2026-02-03 12:41:56 +0100

    changed task 1

--- baranovskyi_hw9.py
+++ baranovskyi_hw9.py
@@ -6,13 +6,13 @@
 #         tmp += str(j)
 #     print(tmp)
 
-# n = int(input('Число: '))
-# x = len(str(n * n))
-# for i in range(1,n+1):
-#     tmp = ''
-#     for j in range(1,n+1):
-#         value = i * j
-#         space = x - len(str(value))
-#         print(' ' * space + str(value), end = ' ')
-#     print(tmp)
+n = int(input('Число: '))
+x = len(str(n * n))
+tmp = ''
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        value = i * j
+        space = x - len(str(value))
+        print(' ' * space + str(value), end = ' ')
+    print(tmp)
 

6fe77b0d8dfad27d94295c8a1e0892b99fc32d90
Ulquiorra-a11y <coll.sasha2013@gmail.com>
2026-02-07 16:20:08 +0100

    added new HW

---
+++ baranovskyi_hw10.py
@@ -0,0 +1,20 @@
+# weights = [50, 10, 5, 2, 1]
+# need = int(input("Введите стоимость: "))
+#
+# for coin in weights:
+#     n = need // coin
+#     if n:
+#         print('Внесите монеты номиналом', coin, ':', n)
+#         need %= coin
+#
+
+numbers = [4, 9, 1, 7, 2, 5, 0, 3, 7, 1, 3]
+print('Изначальный список:', numbers)
+
+for item in numbers:
+    if item % 2:
+        item *= item
+for i in range(len(numbers)):
+    if numbers[i] % 2:
+        numbers[i] = numbers[i] ** 2
+print('Измененный список:', numbers)

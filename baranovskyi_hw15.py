8db0ad17252afe8788e6a8b58e706f4d5cc796f9
Ulquiorra-a11y <coll.sasha2013@gmail.com>
2026-02-26 16:29:10 +0100

    added HW

---
+++ baranovskyi_hw15.py
@@ -0,0 +1,15 @@
+# text_list = ["Hello", "Python Programming", "World", "Advanced Topics", "Simple"]
+# res = []
+# x = " "
+# for item in text_list:
+#     if x not in item:
+#         res.append(item.lower())
+# print(res)
+
+products = [["Laptop", 1200], ["Mouse", 25], ["Keyboard", 75], ["Monitor", 200]]
+discount = 17
+print(f"{'Товар':10} {'Старая цена':15} {'Новая цена':10}")
+for item in products:
+    name, price = item
+    new_price = price-discount*price/100
+    print(f"{name:10} {price:>10.2f}$ {new_price:>13.2f}$")

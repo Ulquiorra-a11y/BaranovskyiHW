b9c3f95412131a198556311fcbd95defdcd5d6fc
Ulquiorra-a11y <coll.sasha2013@gmail.com>
2026-03-15 12:20:34 +0100

    hw added

---
+++ hw19.py
@@ -0,0 +1,38 @@
+# data = {"a": 1, "b": 2, "c": 1, "d": 3}
+# reversed_data = {}
+#
+# for key, value in data.items():
+#     if value not in reversed_data:
+#         reversed_data[value] = []
+#     reversed_data[value].append(key)
+#
+# print("Перевернутый словарь:", reversed_data)
+
+# words = ["anna", "bennet", "john"]
+# result = {word: {letter: word.count(letter) for letter in set(word)} for word in words}
+# print(result)
+
+students = {"Аня": 92, "Боря": 76, "Ваня": 65, "Галя": 48, "Дима": 88, "Ева": 54}
+groups = ["Отличники", "Хорошисты", "Троечники", "Не сдали"]
+res = {}
+
+for group in groups:
+    res[group] = {}
+
+    for name, score in students.items():
+        if score >= 85 and group == "Отличники":
+            res[group][name] = score
+        elif 70 <= score <= 85 and group == "Хорошисты":
+            res[group][name] = score
+        elif 69 >= score >= 50 and group == "Троечники":
+            res[group][name] = score
+        elif 50 > score and group == "Не сдали":
+            res[group][name] = score
+print(res)
+
+
+
+
+
+
+

ce4b2526f2aca404fb0e010be33936d32239adc7
Ulquiorra-a11y <coll.sasha2013@gmail.com>
2026-04-26 10:59:18 +0200

    hw changed

--- hw28.py
+++ hw28.py
@@ -1,4 +1,7 @@
 from itertools import cycle
+from itertools import chain
+from itertools import product
+from typing import Any, Generator
 
 
 '''Напишите программу, которая помогает планировать дела.
@@ -32,8 +35,14 @@ from itertools import cycle
 # vegetables = ["Carrot", "Tomato", "Cucumber"]
 # dairy = ["Milk", "Cheese", "Yogurt"]
 #
-# def all_products(*lists):
-#     return (item.lower() for lst in lists for item in lst)
+# def all_products(*lists: list[str]) -> Generator[Any, Any, None]:
+#     '''
+#     Returns a generator that yields all products from multiple iterables
+#     converted to lowercase
+#     :param lists: lists
+#     :return:product names in lowercase
+#     '''
+#     return (item.lower() for item in chain(*lists))
 #
 # for fruit in all_products(fruits, vegetables, dairy):
 #     print(fruit)
@@ -47,11 +56,15 @@ colors = ["Red", "Blue", "Black"]
 sizes = ["S", "M", "L"]
 
 
-def type_of_clothes(clothes, colors, sizes):
-    return (f"{cloth} - {color} - {size}"
-            for cloth in clothes
-            for color in colors
-            for size in sizes)
+def type_of_clothes(clothes: list[str],colors: list[str],sizes: list[str])-> Generator[str, None, None]:
+    '''
+    Generates all possible combinations of clothes, colors, and sizes
+    :param clothes: list of clothes
+    :param colors: list of colors
+    :param sizes: list of sizes
+    :return: all possible combinations of clothes, colors, and sizes
+    '''
+    return (f"{cloth} - {color} - {size}"for cloth, color, size in product(clothes, colors, sizes))
 
 for item in type_of_clothes(clothes, colors, sizes):
     print(item)

5ba30fa4b7421b7ef66a61c6412c4e8245a980a1
Ulquiorra-a11y <coll.sasha2013@gmail.com>
2026-04-26 10:43:31 +0200

    hw changed

--- hw29.py
+++ hw29.py
@@ -1,6 +1,12 @@
-# def fibonacci():
+# def fibonacci() -> int:
+#     '''
+#     Infinite generator of Fibonacci numbers
+#     :yield: Next number in the Fibonacci sequence
+#     '''
 #     a, b = 0, 1
 #     while True:
+#         if not isinstance(a, int) or not isinstance(b, int):
+#             raise RuntimeError("Fibonacci values must be integers")
 #         yield a
 #         a, b = b, a + b
 #
@@ -12,7 +18,17 @@
 
 data = [3, 1, 2, 3, 4, 1, 5, 2, 6, 7, 5, 8]
 
-def unique_numbers(data):
+def unique_numbers(data: list[int]) -> int:
+    '''
+    Generator that yields unique elements from the list
+    while preserving their original order.
+    :param data: list of integers
+    :yield:unique elements from the list
+    '''
+
+    if not isinstance(data, list):
+        raise TypeError("Input must be a list")
+
     lines = []
 
     for item in data:
